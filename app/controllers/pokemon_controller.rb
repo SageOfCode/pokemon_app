5742f8a4a50bd16a7392994bb0c72d0ec64fa68d
Sage Gonzales <sagegonzales15@gmail.com>
2021-01-27 13:05:55 -0700

    Refactor API call into facade

--- app/controllers/pokemon_controller.rb
+++ app/controllers/pokemon_controller.rb
@@ -1,8 +1,6 @@
 class PokemonController < ApplicationController
   def index
     pokemon = params[:pokemon]
-    conn = Faraday.new("https://pokeapi.co")
-    response = conn.get("/api/v2/pokemon-form/#{pokemon}/")
-    @pokemon = JSON.parse(response.body, symbolize_names: true)
+    @pokemon = PokeFacade.get_a_pokemon(pokemon)
   end
 end
