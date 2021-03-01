96ec4a797a914bbb56ccea0470256b13125b24dc
Sage Gonzales <sagegonzales15@gmail.com>
2021-03-01 14:31:29 -0700

    Add shiny sprite to show page

--- app/poros/pokemon.rb
+++ app/poros/pokemon.rb
@@ -1,11 +1,13 @@
 class Pokemon
   attr_reader :name,
               :number,
-              :image
+              :image,
+              :shiny
 
   def initialize(data)
     @name = data[:name].capitalize
     @number = data[:id]
     @image = data[:sprites][:front_default]
+    @shiny = data[:sprites][:front_shiny]
   end
 end
