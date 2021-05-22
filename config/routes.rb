52d90f4a439d249f80740cc18985d3858063a06e
Sage Gonzales <sagegonzales15@gmail.com>
2021-05-21 22:05:17 -0600

    Add pokemon endpoint route

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "welcome#index"
   resources :pokemon, only: [:index]
+
+  namespace :api do 
+    namespace :v1 do 
+      resources :pokemon, only: :index
+    end
+  end
 end
